fc7b019b99608ace421a9c5083397f81ecef9991
sreehari2003 <sreeharivijaya2003@gmail.com>
2023-01-27 23:14:05 +0530

    feat: added commitlint,husky,pr-template

--- backend/router/router.go
+++ backend/router/router.go
@@ -1,23 +1,18 @@
-package main
+package router
 
 import (
-	"kseb/auth"
-	"kseb/middlewares"
-	"kseb/router"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-
-	auth.Init()
-	router := router.CreateRoute()
-	middlewares.Register(router)
+func CreateRoute() *gin.Engine {
+	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
+			"ok":      true,
 			"message": "api is running on port 8080 succesfully",
 		})
 	})
-	router.Run("localhost:8080")
+	return router
 }
