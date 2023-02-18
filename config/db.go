763152bd909bf9da21ef9c141322e775cb96232d
LEHANS <latheeflathuthekkil@gmail.com>
2023-02-18 23:01:43 +0530

    feat: add db config

---
+++ config/db.go
@@ -0,0 +1,14 @@
+package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect() {
+	db, err := gorm.Open(postgres.Open("postgres://postgres:postgres@localhost:5432/postgres"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate()
+}
