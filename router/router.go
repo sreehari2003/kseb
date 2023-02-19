78accc9ac0f27d26dce9723e68c9c94dd0c6756d
LEHANS <latheeflathuthekkil@gmail.com>
2023-02-19 22:28:01 +0530

    feat: gorm pgsql

--- router/router.go
+++ router/router.go
@@ -3,7 +3,6 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/sreehari2003/kseb/docs"
-	"net/http"
 
 	swaggerFiles "github.com/swaggo/files"
 
@@ -21,23 +20,5 @@ func CreateRoute() *gin.Engine {
 	router := gin.Default()
 	// use ginSwagger middleware to serve the API docs
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	router.GET("/", HealthCheck)
 	return router
-}
-
-// HealthCheck godoc
-// @Summary Show the status of server.
-// @Description get the status of server.
-// @Tags root
-// @Accept */*
-// @Produce json
-// @Success 200 {object} map[string]interface{}
-// @Router / [get]
-func HealthCheck(c *gin.Context) {
-	res := map[string]interface{}{
-		"data": "Server is up and running",
-		"ok":   true,
-	}
-
-	c.JSON(http.StatusOK, res)
 }
