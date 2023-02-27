7a94e9423db130d99a77e16f4dfbc52f90b18aed
Surya-n <90127709+Surya-n@users.noreply.github.com>
2023-02-28 01:09:47 +0530

    feat: implemented auth page (#23)
    * feat:icon
    
    * feat: usestateandhook
    
    * feat: ui of otp page
    
    * feat: added otp,phone number validaton
    
    ---------
    
    Co-authored-by: sreehari2003 <sreeharivijaya2003@gmail.com>

--- web/app/auth/page.tsx
+++ web/app/auth/page.tsx
@@ -1,28 +1,129 @@
-'use client';
-import { Button, Flex, Heading, Input, InputGroup, InputLeftAddon, Text } from '@chakra-ui/react';
+/* eslint-disable react/jsx-props-no-spreading */
 
-const Home = () => (
-  <Flex height={'100vh'} alignItems={'center'} justifyContent={'center'}>
-    <Flex direction={'column'}>
-      <Text fontSize="2xl" mb={10}>
-        Enter Mobile Number
-      </Text>
-      <Text fontSize="1xl" color={'gray'} mb={5}>
-        We will sent you{' '}
-        <Text as="b" color={'black'}>
-          one time password
-        </Text>{' '}
-        on this mobile number
-      </Text>
-      <InputGroup mb={5}>
-        <InputLeftAddon children="+91-" />
-        <Input variant="flushed" />
-      </InputGroup>
-      <Button size="md" height="48px" width="-moz-fit-content" border="2px" colorScheme="blue">
-        GET OTP
-      </Button>
-    </Flex>
-  </Flex>
-);
+'use client';
+
+import {
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Text,
+  Icon,
+  Box,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from '@chakra-ui/react';
+import { MdOutlineMessage } from 'react-icons/md';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useState } from 'react';
+import { phoneNumber, OTP } from '@app/views/validator';
+
+type Phone = Yup.InferType<typeof phoneNumber>;
+type Otp = Yup.InferType<typeof OTP>;
+
+const Home = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Phone>({
+    mode: 'onSubmit',
+    resolver: yupResolver(phoneNumber),
+  });
+
+  const {
+    register: registerOTP,
+    handleSubmit: handleOtpSubmit,
+    formState: { errors: otpErrors },
+  } = useForm<Otp>({
+    mode: 'onSubmit',
+    resolver: yupResolver(OTP),
+  });
+
+  const [isOtpSent, setOtpSent] = useState<boolean>(false);
+
+  const phoneSubmit: SubmitHandler<Phone> = (data) => {
+    setOtpSent(true);
+    console.log(Number(data.phoneNumber));
+  };
+
+  const otpSubmit: SubmitHandler<Otp> = (data) => {
+    console.log(Number(data.otp));
+  };
+
+  return (
+    <Box>
+      <Flex height="25vh" alignItems="center" justifyContent="center" mt={10}>
+        <Icon as={MdOutlineMessage} w={20} h={20} />
+      </Flex>
+      {!isOtpSent && (
+        <Flex height="75vh" alignItems="Flex-start" justifyContent="center">
+          <Flex direction="column">
+            <Text as="b" color="black" fontSize="2xl" mb={10} textAlign="center">
+              Enter Mobile Number
+            </Text>
+            <Text fontSize="1xl" color="gray" mb={10}>
+              We will sent you{' '}
+              <Text as="b" color="black">
+                one time password
+              </Text>{' '}
+              on this mobile number
+            </Text>
+            <form onSubmit={handleSubmit(phoneSubmit)}>
+              <FormControl isInvalid={!!errors.phoneNumber}>
+                <FormLabel>Phone number</FormLabel>
+                <InputGroup>
+                  <InputLeftAddon>+91</InputLeftAddon>
+                  <Input type="tel" {...register('phoneNumber')} />
+                </InputGroup>
+                <FormErrorMessage>Phone number is not valid</FormErrorMessage>
+                <Button
+                  mt="10px"
+                  type="submit"
+                  size="md"
+                  height="48px"
+                  width="100%"
+                  border="2px"
+                  colorScheme="purple"
+                >
+                  GET OTP
+                </Button>
+              </FormControl>
+            </form>
+          </Flex>
+        </Flex>
+      )}
+      {isOtpSent && (
+        <Flex alignItems="center" direction="column">
+          <Text as="b" mb={10} textAlign="end">
+            Please Enter the <b>OTP</b>
+          </Text>
+          <form onSubmit={handleOtpSubmit(otpSubmit)}>
+            <FormControl isInvalid={!!otpErrors.otp} w="300px">
+              <FormLabel>OTP</FormLabel>
+              <Input {...registerOTP('otp')} placeholder="Enter your OTP" />
+              <FormErrorMessage w="100%">Please Enter a valid otp</FormErrorMessage>
+              <Button
+                mt="10px"
+                type="submit"
+                size="md"
+                height="48px"
+                border="2px"
+                colorScheme="purple"
+                w="100%"
+              >
+                verify
+              </Button>
+            </FormControl>
+          </form>
+        </Flex>
+      )}
+    </Box>
+  );
+};
 
 export default Home;
---
+++ web/views/validator/auth.ts
@@ -0,0 +1,16 @@
+import * as Yup from 'yup';
+
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+export const phoneNumber = Yup.object({
+  phoneNumber: Yup.string()
+    .required()
+    .min(10)
+    .max(10)
+    .matches(phoneRegExp, 'Phone number is not valid'),
+});
+
+export const OTP = Yup.object({
+  otp: Yup.string().min(4).max(4).required('Please Enter a valid otp'),
+});
