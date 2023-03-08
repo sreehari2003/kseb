540241d1837a496e641855d7c0f78365a01ff280
sreehari2003 <sreeharivijaya2003@gmail.com>
2023-03-08 13:51:36 +0530

    fix: changed to pages dir from app dir

---
+++ web/pages/_app.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { Child } from '@app/types';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+
+type ComponentWithPageLayout = AppProps & {
+  Component: AppProps['Component'] & {
+    Layout?: (arg: Child) => JSX.Element;
+  };
+};
+
+const RootLayout = ({ Component, pageProps }: ComponentWithPageLayout) => (
+  <ChakraProvider>
+    {Component.Layout ? (
+      <Component.Layout>
+        <Component {...pageProps} />
+      </Component.Layout>
+    ) : (
+      <Component {...pageProps} />
+    )}
+  </ChakraProvider>
+);
+
+export default RootLayout;
