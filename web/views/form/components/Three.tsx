04081701588eb4ad2f44b29c2fd4677d98755130
unknown <suryaparvathy2001@gmail.com>
2023-04-12 13:26:42 +0530

    fix: css

--- web/views/form/components/Three.tsx
+++ web/views/form/components/Three.tsx
@@ -1,23 +1,27 @@
-import { Flex, Text, FormControl, FormLabel, Input, Stack, Select, Box } from '@chakra-ui/react';
+import { Flex, Text, FormControl, FormLabel, Input, Stack, Box } from '@chakra-ui/react';
 import React from 'react';
-
+import { Select } from 'chakra-react-select';
+const time = [
+  { label: 'AM', value: 'AM' },
+  { label: 'PM', value: 'PM' },
+];
 export const Three = () => (
-  <Box p={5}>
-    <Flex alignItems="Flex-start" justifyContent="center">
-      <Flex direction="column" w="70%">
+  <Box>
+    <Flex justifyContent="center" w="800px">
+      <Flex direction="column">
         <Text mt="5">Safety arrangements are completed and permission to work is granted</Text>
 
         <FormControl id="input1" mb={4} mt={10}>
           <FormLabel>Name of the officer/designation</FormLabel>
-          <Input variant="flushed" />
+          <Input />
           <Text>We engage in bespoke work</Text>
         </FormControl>
-        <Flex justifyContent="space-around">
+        <Flex justifyContent="space-between">
           <Text>Name of employees/designation </Text>
           <Text>Time</Text>
           <Text>Safety equipment used(ladder,gloves,safety belt,etc)</Text>
         </Flex>
-        <Flex justifyContent="space-around">
+        <Flex justifyContent="space-between">
           <Stack spacing={7}>
             <Text>1</Text>
             <Text>2</Text>
@@ -43,12 +47,12 @@ export const Three = () => (
             <Input placeholder="Time" size="md" />
           </Stack>
           <Stack spacing={3}>
-            <Select placeholder=" AM" size="md" />
-            <Select placeholder="AM" size="md" />
-            <Select placeholder="AM" size="md" />
-            <Select placeholder="AM" size="md" />
-            <Select placeholder="AM" size="md" />
-            <Select placeholder="AM" size="md" />
+            <Select placeholder="AM" options={time} size="md" />
+            <Select placeholder="AM" options={time} size="md" />
+            <Select placeholder="AM" options={time} size="md" />
+            <Select placeholder="AM" options={time} size="md" />
+            <Select placeholder="AM" options={time} size="md" />
+            <Select placeholder="AM" options={time} size="md" />
           </Stack>
           <Stack spacing={3}>
             <Input size="md" />
