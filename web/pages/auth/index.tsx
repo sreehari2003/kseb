57762e05e47adce1ec64f8163e3e1e805df47865
sreehari2003 <sreeharivijaya2003@gmail.com>
2023-04-15 12:41:55 +0530

    feat: added otp handling functions

--- web/pages/auth/index.tsx
+++ web/pages/auth/index.tsx
@@ -11,13 +11,20 @@ import {
   Box,
   FormControl,
   FormLabel,
+  useToast,
   FormErrorMessage,
 } from '@chakra-ui/react';
 import { MdOutlineMessage } from 'react-icons/md';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import {
+  createCode,
+  resendCode,
+  consumeCode,
+  getLoginAttemptInfo,
+} from 'supertokens-auth-react/recipe/passwordless';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { phoneNumber, OTP } from '@app/views/validator';
 
 type Phone = Yup.InferType<typeof phoneNumber>;
@@ -32,7 +39,9 @@ const Home = () => {
     mode: 'onSubmit',
     resolver: yupResolver(phoneNumber),
   });
+  const toast = useToast();
 
+  const hasInitialOTPBeenSent = async () => (await getLoginAttemptInfo()) !== undefined;
   const {
     register: registerOTP,
     handleSubmit: handleOtpSubmit,
@@ -42,15 +51,167 @@ const Home = () => {
     resolver: yupResolver(OTP),
   });
 
-  const [isOtpSent, setOtpSent] = useState<boolean>(false);
+  // this state is used to determine if the user has recieved the otp and is ready to enter the otp
+  const [isOTPscreenisVisible, setOTPscreenisVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      const loginAttemptInfo = await hasInitialOTPBeenSent();
+      setOTPscreenisVisible(loginAttemptInfo);
+    })();
+  }, [handleSubmit]);
+
+  const sendOTP = async (phone: string): Promise<void> => {
+    try {
+      await createCode({
+        phoneNumber: `+91${phone}`,
+      }); // OTP sent successfully.
+
+      toast({
+        title: 'OTP sent successfully',
+        description: 'Please check your Phone for an OTP',
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+      });
+      // 'Please check your Phone for an OTP')
+      setOTPscreenisVisible(true);
+    } catch (err: any) {
+      if (err.isSuperTokensGeneralError === true) {
+        // this may be a custom error message sent from the API by you,
+        // or if the input email / phone number is not valid.
+        toast({
+          title: "Couldn't send OTP",
+          description: 'something went wrong while trying to send otp',
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        });
+      } else {
+        //  window.alert('Oops! Something went wrong.');
+        toast({
+          title: "Couldn't send OTP",
+          description: 'something went wrong while trying to send otp',
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    }
+  };
+
+  const resendOTP = async () => {
+    try {
+      const response = await resendCode();
+
+      if (response.status === 'RESTART_FLOW_ERROR') {
+        // this can happen if the user has already successfully logged in into
+        // another device whilst also trying to login to this one.
+        //  'Login failed. Please try again');
+        toast({
+          title: "Couldn't send OTP",
+          description: 'something went wrong while trying to send otp',
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        });
+      } else {
+        // OTP resent successfully.
+        // 'Please check your phone for the OTP');
+        toast({
+          title: 'OTP resent succesfully',
+          description: 'Please check your phone for the OTP',
+          status: 'success',
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    } catch (err: any) {
+      if (err.isSuperTokensGeneralError === true) {
+        toast({
+          title: "Couldn't send OTP",
+          description: 'something went wrong while trying to send otp',
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        });
+        // this may be a custom error message sent from the API by you.
+      } else {
+        //         window.alert('Oops! Something went wrong.');
+        toast({
+          title: "Couldn't send OTP",
+          description: 'something went wrong while trying to send otp',
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    }
+  };
+
+  // used to verify the otp
+  const handleOTPInput = async (otp: string) => {
+    try {
+      const response = await consumeCode({
+        userInputCode: otp,
+      });
+      localStorage.removeItem('supertokens-passwordless-loginAttemptInfo');
+
+      if (response.status === 'OK') {
+        toast({
+          title: 'Login Successfull',
+          status: 'success',
+          duration: 3000,
+          isClosable: true,
+        });
+        // an api call will be automatically sent from context to check the user in db
+        // doesSessionExist is a dependency in useEffect in authCOntext
+      } else if (response.status === 'INCORRECT_USER_INPUT_CODE_ERROR') {
+        // the user entered an invalid OTP
+        toast({
+          title: 'Incorrect OTP',
+          description: 'The enterd OTP is wrong please try again',
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        });
+      } else if (response.status === 'EXPIRED_USER_INPUT_CODE_ERROR') {
+        // it can come here if the entered OTP was correct, but has expired because
+        // it was generated too long ago.
+        toast({
+          title: 'OTP Expired',
+          description: 'Old OTP entered , Please regenerate a new one and try again',
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        });
+      } else {
+        // this can happen if the user tried an incorrect OTP too many times.
+        toast({
+          title: 'Login Failed',
+          description: 'multiple incorrect otp entry, please try again later',
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    } catch (err: any) {
+      toast({
+        title: 'Something went wrong',
+        description: 'Please try again later',
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
 
   const phoneSubmit: SubmitHandler<Phone> = (data) => {
-    setOtpSent(true);
-    console.log(Number(data.phoneNumber));
+    sendOTP(data.phoneNumber);
   };
 
   const otpSubmit: SubmitHandler<Otp> = (data) => {
-    console.log(Number(data.otp));
+    handleOTPInput(data.otp);
   };
 
   return (
@@ -58,7 +219,7 @@ const Home = () => {
       <Flex h="250" alignItems="center" justifyContent="center" mt={10}>
         <Icon as={MdOutlineMessage} w={20} h={20} />
       </Flex>
-      {!isOtpSent && (
+      {!isOTPscreenisVisible && (
         <Flex alignItems="Flex-start" justifyContent="center">
           <Flex direction="column">
             <Text as="b" color="black" fontSize="2xl" mb={10} textAlign="center">
@@ -95,7 +256,7 @@ const Home = () => {
           </Flex>
         </Flex>
       )}
-      {isOtpSent && (
+      {isOTPscreenisVisible && (
         <Flex alignItems="center" direction="column">
           <Text as="b" mb={10} textAlign="end">
             Please Enter the <b>OTP</b>
@@ -103,6 +264,13 @@ const Home = () => {
           <form onSubmit={handleOtpSubmit(otpSubmit)}>
             <FormControl isInvalid={!!otpErrors.otp} w="300px">
               <FormLabel>OTP</FormLabel>
+              <FormLabel
+                onClick={resendOTP}
+                color="#0075E4"
+                _hover={{ cursor: 'pointer', textDecoration: 'underline' }}
+              >
+                Resend SMS
+              </FormLabel>
               <Input {...registerOTP('otp')} placeholder="Enter your OTP" />
               <FormErrorMessage w="100%">Please Enter a valid otp</FormErrorMessage>
               <Button
