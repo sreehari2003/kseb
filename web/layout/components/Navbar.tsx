3cdae16fc4fc58543fdac887467e3c98daa6ee37
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2023-04-24 10:18:33 +0530

    feat: added logo

--- web/layout/components/Navbar.tsx
+++ web/layout/components/Navbar.tsx
@@ -1,47 +1,31 @@
-import { Box, Button, Flex, Heading, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { Box, Flex, Heading, Input, InputGroup, InputLeftElement, Image } from '@chakra-ui/react';
 import React from 'react';
 import { BsSearch } from 'react-icons/bs';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-export const Navbar = () => {
-  const router = useRouter();
-
-  const handleLoginClick = () => {
-    router.push('/auth');
-  };
-  return (
-    <Flex
-      p="7"
-      height="90px"
-      backgroundColor="teal"
-      justifyContent="space-between"
-      boxShadow="md"
-      alignItems="center"
-    >
-      <Link href="/">
-        <Heading
-          fontSize="30px"
-          fontWeight="400"
-          _hover={{ cursor: 'pointer', shadow: 'md' }}
-          transition="2ms"
-        >
-          Suraksha
-        </Heading>
-      </Link>
-      <Box>
-        <InputGroup w="350px">
-          <InputLeftElement pointerEvents="none">
-            <BsSearch />
-          </InputLeftElement>
-          <Input type="string" placeholder="search" bg="white" />
-        </InputGroup>
-      </Box>
-      <Box>
-        <Button onClick={handleLoginClick} colorScheme="teal" borderColor="teal">
-          Log in
-        </Button>
-      </Box>
-    </Flex>
-  );
-};
+export const Navbar = () => (
+  <Flex
+    p="7"
+    height="90px"
+    backgroundColor="teal"
+    justifyContent="space-between"
+    boxShadow="md"
+    alignItems="center"
+  >
+    <Link href="/">
+      <Image src="/logo.png" w="200px" />
+    </Link>
+    <Box>
+      <InputGroup w="350px">
+        <InputLeftElement pointerEvents="none">
+          <BsSearch />
+        </InputLeftElement>
+        <Input type="string" placeholder="search" bg="white" />
+      </InputGroup>
+    </Box>
+    <Box>
+      <Heading>fb</Heading>
+    </Box>
+  </Flex>
+);
