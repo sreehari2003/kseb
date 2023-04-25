77c7ca49242d6307669a1391bf42625ed3458870
Arafath987 <94624401+Arafath987@users.noreply.github.com>
2023-04-25 20:24:13 +0530

    dashboard page craeted

---
+++ web/pages/dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NextPageWithLayout } from 'next';
+import { BaseLayout } from '@app/layout';
+
+const Dashboard:NextPageWithLayout = () => {
+  return (
+    <div>
+        
+      
+    </div>
+  )
+}
+Dashboard.Layout=BaseLayout
+export default Dashboard
