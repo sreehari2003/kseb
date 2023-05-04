a6e6b44ecccbc42c5f22e9fc21715c3013f16a39
unknown <suryaparvathy2001@gmail.com>
2023-05-04 12:25:48 +0530

    feat: profile page

---
+++ web/views/dashboard/components/Profile.tsx
@@ -0,0 +1,12 @@
+import { FormLabel, Box, FormControl, Input } from '@chakra-ui/react';
+import React from 'react';
+export const Profile = () => (
+  <Box p="6">
+    <form>
+      <FormControl id="name">
+        <FormLabel>Name</FormLabel>
+        <Input type="text" />
+      </FormControl>
+    </form>
+  </Box>
+);
