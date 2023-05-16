07fa47e7ddcdc63db42c9bb5afe88ddc5b188731
unknown <suryaparvathy2001@gmail.com>
2023-05-16 21:27:56 +0530

    feat: wrote post request in form

--- web/pages/[id].tsx
+++ web/pages/[id].tsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { useState } from 'react';
-import { Center } from '@chakra-ui/react';
+import { Center, useToast } from '@chakra-ui/react';
 import { useForm, FormProvider, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { InferType } from 'yup';
 import { One, Two, NavigationButtons, Three } from '@app/views/form';
 import { FinalForm, StepByStepForm } from '@app/views/validator';
 import { BaseLayout } from '@app/layout';
+import { surakshaAPI } from '@app/config';
 
 type FormType = InferType<typeof FinalForm>;
 
 const Home = () => {
+  const toast = useToast();
   const [currentStep, setCurrentStep] = useState<number>(1);
   const isReadyForSubmission = currentStep === 3;
   const methods = useForm<FormType>({
@@ -25,13 +27,35 @@ const Home = () => {
     setCurrentStep((el) => el - 1);
   };
 
-  const onSubmit: SubmitHandler<FormType> = (data) => {
+  const onSubmit: SubmitHandler<FormType> = async (datas) => {
     if (isReadyForSubmission) {
       // send post request
-      console.log(data);
+      try {
+        const { data } = await surakshaAPI.post('/form', datas);
+        if (!data.ok) {
+          throw new Error();
+        } else {
+          toast({
+            title: 'Form Submission Successfull.',
+            description: 'Form was submitted successfully',
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          });
+        }
+      } catch {
+        toast({
+          title: 'Form Submission failed.',
+          description: 'Something went wrong while submitting form',
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        });
+      }
     } else {
       nextStep();
     }
+    console.log(datas);
   };
   return (
     <Center>
