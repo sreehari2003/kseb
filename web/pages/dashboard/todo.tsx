fe38546ffc9bb35cc406bb429516098035714356
Arafath987 <94624401+Arafath987@users.noreply.github.com>
2023-06-11 21:13:37 +0530

    feat:card created

--- web/pages/dashboard/todo.tsx
+++ web/pages/dashboard/todo.tsx
@@ -6,7 +6,6 @@ import { TodoCard } from '@app/views/dashboard';
 
 const Todo: NextPageWithLayout = () => (
   <Box>
-    <Flex flexDirection="row">
       <Heading
         mt="20px"
         ml="20px"
@@ -17,31 +16,7 @@ const Todo: NextPageWithLayout = () => (
         height="71.13px"
       >
         Todo list
-      </Heading>
-      <Button
-        width="180px"
-        height="40px"
-        colorScheme="#FFFDFD"
-        size="md"
-        mt="20px"
-        ml="300px"
-        borderRadius="13px"
-        border="2px"
-        borderColor="#D1D1D180"
-      >
-        <Heading
-          mt="20px"
-          color="#418E9E"
-          lineHeight="44px"
-          fontSize="xl"
-          fontWeight="medium"
-          width="438px"
-          height="71.13px"
-        >
-          + New Task
-        </Heading>
-      </Button>
-    </Flex>
+      </Heading> 
     <Grid ml="10px" mt="-20px" alignItems="center" justifyContent="center">
       <GridItem w="950px" h="3px" bg="#D1D1D180" />
     </Grid>
